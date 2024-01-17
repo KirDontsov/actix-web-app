85741c342508be214f09651e032800b6e1b82a69
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-01-17 22:54:20 +0300

    fix: comments

--- src/controllers/config.rs
+++ src/controllers/config.rs
@@ -10,21 +10,27 @@ use crate::controllers::user::*;
 
 pub fn config(conf: &mut web::ServiceConfig) {
 	let scope = web::scope("/api")
+		// auth
 		.service(register_handler)
 		.service(login_handler)
 		.service(get_me_handler)
 		.service(logout_handler)
+		// parsers
 		.service(firms_crawler_handler)
 		.service(firms_info_crawler_handler)
 		.service(firms_reviews_crawler_handler)
+		//user
 		.service(get_users_handler)
 		.service(get_user_handler)
 		.service(update_user_handler)
+		// quote
 		.service(get_quotes_handler)
 		.service(get_quote_handler)
 		.service(add_quote_handler)
+		// firm
 		.service(get_firms_handler)
 		.service(get_firm_handler)
+		//review
 		.service(get_reviews_handler);
 
 	conf.service(scope);
