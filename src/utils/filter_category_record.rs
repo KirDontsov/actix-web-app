5be96ca722ecd59067b0146d85b84df14cfb2cd3
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2024-09-26 15:12:45 +0300

    feat: add order for cities and categories
    RR-999

--- src/utils/filter_category_record.rs
+++ src/utils/filter_category_record.rs
@@ -8,6 +8,7 @@ pub fn filter_category_record(category: &Category) -> FilteredCategory {
 		single_name: category.single_name.to_owned(),
 		rod_name: category.rod_name.to_owned(),
 		pred_name: category.pred_name.to_owned(),
+		vin_name: category.vin_name.to_owned(),
 		order_number: category.order_number.to_owned(),
 		is_active: category.is_active.to_owned(),
 	}
