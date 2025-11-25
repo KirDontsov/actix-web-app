52a12a159fdbb4a3d63c2f425014584a1fcf3460
Кирилл Донцов <kirill.dontsov@kmslh.com>
2025-11-25 18:00:30 +0300

    fix: cookie secure
    RR-008

--- src/controllers/auth/login.rs
+++ src/controllers/auth/login.rs
@@ -61,11 +61,11 @@ async fn login_handler(
 	.unwrap();
 
 	let cookie = Cookie::build("token", token.to_owned())
-		.same_site(SameSite::Lax) // SameSite::Lax for Safari compatibility
+		.same_site(SameSite::None) // SameSite::Lax for Safari compatibility
 		.path("/")
 		.max_age(ActixWebDuration::new(60 * 60, 0))
 		.http_only(true)
-		.secure(false)
+		.secure(true)
 		.finish();
 
 	HttpResponse::Ok()
